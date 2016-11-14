666c5bc76ac104670d1d9fbfceb2c8d38b2156fb
Abhijeet Aute <aaute@jcp.com>
2016-11-14 19:10:11 +0530

    modified pom.xml

--- src/test/java/com/javacodegeeks/testng/maven/TestNgMavenSecondClass.java
+++ src/test/java/com/javacodegeeks/testng/maven/TestNgMavenSecondClass.java
@@ -1,11 +1,24 @@
 package com.javacodegeeks.testng.maven;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class TestNgMavenSecondClass {
 
 	@Test
-	public void oneMoreTest() {
-		System.out.println("This is a TestNG-Maven based test");
+	public void oneMoreTest() throws InterruptedException {
+	
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://www.google.com");
+		
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.name("q")).sendKeys("Abhijeet Aute");
+		
+		Thread.sleep(3000);
+		
+		driver.findElement(By.name("btnG")).click();
 	}
 }
